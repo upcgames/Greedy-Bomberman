ba160f6c816bd8c5068fae9313a157821c85f1d5
Diego Jara <saintplay96@gmail.com>
2017-06-27 13:23:16 -0500

    Agrega Estados a robot

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -3,27 +3,40 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 
+public enum RobotEstado {
+	Observando,
+	Escapando,
+	Atacando
+}
+
 public class Robot : Jugador {
 	
-	public LayerMask solidos_capa;
-	
+	private Collider _collider;
+
+	public RobotEstado estado;
+
 	public override void Start() {
 		 base.Start();
 		 Bomba.cuandoSePlanta += this.reaccionarABombas;
+		 this._collider = this.GetComponent<Collider>(); 
+	}
+
+	void Escapar() {
 	}
 	
 	private void reaccionarABombas() {
 		GameObject[] bombas = GameObject.FindGameObjectsWithTag("Bomb");
-		Bomba bomba = bombas[0].GetComponent<Bomba>();
+		Bomba bomba = bombas[bombas.Length - 1].GetComponent<Bomba>();
 
-		RaycastHit hit;
-		Ray ray = new Ray(bomba.transform.position, Vector3.back);
-		Debug.DrawLine(ray.origin, ray.origin + Vector3.back, Color.red, .5f);
-
-		if (Physics.Raycast(ray, out hit, bomba.longitud_propagacion, solidos_capa)) {
-			if (hit.collider.tag == "Player") {
-				Debug.Log("¿Alo?, robot al habla! una bomba ha sido plantada!!"+ bombas.Length);
-			}
+		RaycastHit golpe;
+		if (
+			this._collider.Raycast(new Ray(bomba.transform.position, Vector3.forward), out golpe, bomba.longitud_propagacion) ||
+			this._collider.Raycast(new Ray(bomba.transform.position, Vector3.right), out golpe, bomba.longitud_propagacion) ||
+			this._collider.Raycast(new Ray(bomba.transform.position, Vector3.back), out golpe, bomba.longitud_propagacion) ||
+			this._collider.Raycast(new Ray(bomba.transform.position, Vector3.left), out golpe, bomba.longitud_propagacion)
+			)
+		{
+			this.Escapar();
 		}
 	}
 }
